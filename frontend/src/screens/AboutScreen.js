da40274f9e01b1a7de1dc48c75de45bc7f841d3a
unknown <demonking0010@gmail.com>
2022-11-29 15:42:08 -0800

    commit

---
+++ frontend/src/screens/AboutScreen.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import "../styles/AboutScreen.css"
+
+export const AboutScreen = () => {
+  return (
+    <div className="AboutScreen">
+      <div className="about_us">
+        {/* <h2>E-Pharm</h2> */}
+        <div className="mission">
+          <span><h1 styles={{color:'red'}}>FOOD HUB</h1></span>  
+          <p>
+            <h6>Our mission is to transport DELISIOUS FOOD with high speed and good
+            quality.{" "}</h6>
+          </p>
+        </div>
+
+        <span><h1>About us</h1></span>
+        <img src="https://www.justgotochef.com/content/images/about-us-banner.jpg" alt="" />
+        <br /> <br /> <br />
+        <p>
+          <h4>
+          Everything began in 1946 when the proprietor opened his first espresso and doughnut and sandwich shop. It was gigantically fruitful and this drove him to begin another shop called organization name. He opened the principal store in 1950 in Massachusetts in the USA.Today, ‘Organization Name’ can be found in 30 nations worldwide in excess of 10,000 areas. It has turned into the world’s driving’ Coffee and Baked Goods chain, serving in excess of 900 million doughnuts and 1.5 some espresso a year all-inclusive. Furthermore, presently it’s additionally in ____.
+
+At ‘Organization Name’, you can appreciate a wide determination of doughnuts, espresso, drinks, and sandwiches, that will have you sipping and slurping and gorgin’ and more.
+          </h4>
+        </p>
+        <br /> <br /> <br />
+
+        <span><h1>motivation</h1></span>
+
+        <img src="https://previews.123rf.com/images/dang2557/dang25571809/dang2557180900040/107827437-inspirational-and-motivation-quote-on-food-and-drink-background.jpg" alt="" />
+        <br /> <br /> <br />
+        <p>
+          <h4>
+
+          Waiting in lines in front of food trucks is a common experience for Columbia students,
+          and not surprisingly, same thing is happening for many other universities and companies!
+          Sometimes it will take even more than 30 minutes to get lunch, because apart form the
+          time spent waiting in line, waiting for cooking can also be extremely slow. Waiting
+          itself is already frustrating, not to mention if we have to wait in cold, rainy, windy
+          or snowy days!
+
+          Of course, we do have some other options, but they are far from ideal as well. If we
+          bring our own lunch to university, by the time for lunch it will be as cold as stone.
+          Moreover, not everyone is skillful and patient enough for cooking! Alternatively we can
+          order food online or by phone and hope it will arrive just in time. However, this is
+          just nearly never the case. I believe you must have similar experience: you have to
+          answer a phone call in the middle of a class or meeting telling you your food has
+          arrived. Or you waited hungrily for 15 minutes until the delivery finally called you.
+          Why don’t we have a more reliable choice? A choice that can keep you away from
+          waiting as well as keep your lunch hot?
+
+          These are the motivation of our solution: smart food delivery! When people can fetch their lunch
+          from our smart food delivery boxes, problem can thus be solved. Moreover, the hygiene and
+          security condition outside campus will be improved as well, without so many people jamming in
+          front of food trucks!
+          </h4>
+        </p>
+        <br /> <br /> <br />
+
+        <span><h1>OUR COMMITMENT</h1></span>
+        <img src="https://www.shutterstock.com/image-illustration/3d-illustration-rubber-stamp-text-260nw-1329389570.jpg" alt="" />
+        <br /> <br /> <br />
+        <p>
+          <h4>
+          We believe that delivering a stable supply of safe, delicious food is the key mission of the
+          FoodHub.
+
+          The FoodHub regards food safety as the top management priority. This is because we believe it is
+          more important than anything that the food ingredients customers eat not only taste delicious but
+          can be enjoyed with peace of mind. This is why FoodHub established the Group Food Safety Pursuing
+          Division, which conducts daily on-site research and inspections in all processes from procurement
+          through the provision of services, to ensure food safety.
+          </h4>
+        </p>
+        <br /> <br /> <br />
+      </div>
+    </div>
+  )
+}
+export default AboutScreen
